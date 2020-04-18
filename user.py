b9c1d333db2a61ce2423271646f0cf18988bdf47
hamisicodes <newhamisi@gmail.com>
2020-04-18 22:21:18 +0300

    add a static variable userslist and define a method to append an object into the users list

--- user.py
+++ user.py
@@ -1,5 +1,11 @@
 class User:
+
+    users_list = []
+
     def __init__(self,username,password):
         self.username = username
         self.password = password
+
+    def saveUser(self):
+        User.users_list.append(User)
         
