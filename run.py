aed9078d6447497021b9076067202281865a3425
hamisicodes <newhamisi@gmail.com>
2020-04-19 12:10:21 +0300

    add functions to create and save users in the run/py file

--- run.py
+++ run.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3.6
+from user import User
+from credentials import  Credential
+
+def create_user_account(user_name,passcode):
+    new_user = User(user_name,passcode)
+    return new_user
+
+def save_user(user):
+    user.save_user()
+
+
+
+
