4f7ed62a64caebce289ba0d0ec1b898cb09fe37a
hamisicodes <newhamisi@gmail.com>
2020-04-19 12:21:56 +0300

    create a main function and add some small chunks of code

--- run.py
+++ run.py
@@ -12,3 +12,22 @@ def save_user(user):
 
 
 
+
+
+def main():
+    print('Hello Welcome to the PASSWORD LOCKER APP')
+    print('Use the short codes:\n')
+    print('#'*80)
+    print()
+    print('L -> to log into your existing PASSWORD LOCKER account')
+    print('C -> to create a new PASSWORD LOCKER account\n')
+    print('#'*80)
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    main()
