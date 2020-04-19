53b30ae52601c320f3c5c2887789b74e0488efbf
hamisicodes <newhamisi@gmail.com>
2020-04-19 10:52:53 +0300

    add test method to save credential

--- test_credentials.py
+++ test_credentials.py
@@ -4,7 +4,7 @@ from credentials import Credential
 class test_credential(unittest.TestCase):
 
     def setUp(self):
-        
+
          self.new_credential = Credential("twitter","twitter001","hamisi")
 
     def test_init(self):
@@ -12,6 +12,12 @@ class test_credential(unittest.TestCase):
         self.assertEqual(self.new_credential.password,"twitter001")
         self.assertEqual(self.new_credential.account,"twitter")
         self.assertEqual(self.new_credential.username,"hamisi")
+
+    def test_save_credentials(self):
+        self.new_credential.savecredential()
+
+        self.assertEqual(len(Credential.credential_list),1)
+    
 
 
 
